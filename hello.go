100568221fa0005d00dd4c56aefc603df3c2703a
Suryajith Sujith <suryajithsujith@gmail.com>
2025-07-09 23:30:26 +0530

    added new file and changes to hello

--- hello.go
+++ hello.go
@@ -1,6 +1,12 @@
-package main
-import "fmt"
+package main   //defined a package
+import "fmt" //importing the fmt package for formatted I/O
 
 func main() {
   fmt.Println("Hello, World!")
 }
+
+// our main function which acts as an entry point for our application, just like in other languages like C, Java, or C#.
+//to run our code, we can simply use go run command - go run hello.go
+// or we can build our code into an executable using go build command - go build hello.go
+// this will create an executable file named hello in the current directory, which we can run using
+// hello.exe on Windows.
